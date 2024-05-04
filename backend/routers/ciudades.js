4c9092bdd08dca3c8eafc89aa756436661c54719
JoSeLoDiAz <josediazd40z@gmail.com>
2024-05-04 13:08:45 -0500

    probando que suba mis cambios por JOSELO

--- backend/routers/ciudades.js
+++ backend/routers/ciudades.js
@@ -1,44 +1,44 @@
-import { validateFields } from "../valichecks/validate-fields.js";
-import {check} from "express-validator"
-import { Router } from "express"
-import { isAdmin } from "../helpers/validate-user.js";
+// import { validateFields } from "../valichecks/validate-fields.js";
+// import {check} from "express-validator"
+// import { Router } from "express"
+// import { isAdmin } from "../helpers/validate-user.js";
 
-const router= Router()
+// const router= Router()
 
 
-import {
-postCiudad,
-getCiudad,
-getCiudadCodigo,
-putCiudad,
-patchCiudad,
-deleteCiudad
-} from '../controllers/ciudades.js';
+// import {
+// postCiudad,
+// getCiudad,
+// getCiudadCodigo,
+// putCiudad,
+// patchCiudad,
+// deleteCiudad
+// } from '../controllers/ciudades.js';
 
-router.post("/",[
-    check("nombre","Ingrese el campo Nombre").trim().not().isEmpty().toLowerCase(),
-    check("daneciudad","Ingrese el campo Codino DANE Ciudad con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
-    check("region","Ingrese el campo Region").trim().not().isEmpty().toLowerCase(),
-    check("departamento","Ingrese el campo Departamento").trim().not().isEmpty().toLowerCase(),
-    check("danedepartamento","Ingrese el campo Codigo DANE Departamento con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
-    validateFields
-],postCiudad)
+// router.post("/",[
+//     check("nombre","Ingrese el campo Nombre").trim().not().isEmpty().toLowerCase(),
+//     check("daneciudad","Ingrese el campo Codino DANE Ciudad con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
+//     check("region","Ingrese el campo Region").trim().not().isEmpty().toLowerCase(),
+//     check("departamento","Ingrese el campo Departamento").trim().not().isEmpty().toLowerCase(),
+//     check("danedepartamento","Ingrese el campo Codigo DANE Departamento con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
+//     validateFields
+// ],postCiudad)
 
-router.get('/', getCiudad);
+// router.get('/', getCiudad);
 
-router.get('/:codigo',getCiudadCodigo);
+// router.get('/:codigo',getCiudadCodigo);
 
-router.put('/:id',[
-    check("nombre","Ingrese el campo Nombre").trim().not().isEmpty().toLowerCase(),
-    check("daneciudad","Ingrese el campo Codino DANE Ciudad con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
-    check("region","Ingrese el campo Region").trim().not().isEmpty().toLowerCase(),
-    check("departamento","Ingrese el campo Departamento").trim().not().isEmpty().toLowerCase(),
-    check("danedepartamento","Ingrese el campo Codigo DANE Departamento con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
-    validateFields
-], putCiudad);
+// router.put('/:id',[
+//     check("nombre","Ingrese el campo Nombre").trim().not().isEmpty().toLowerCase(),
+//     check("daneciudad","Ingrese el campo Codino DANE Ciudad con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
+//     check("region","Ingrese el campo Region").trim().not().isEmpty().toLowerCase(),
+//     check("departamento","Ingrese el campo Departamento").trim().not().isEmpty().toLowerCase(),
+//     check("danedepartamento","Ingrese el campo Codigo DANE Departamento con minimo 5 digitos").trim().not().isEmpty().isLength({min:5,max:8}),
+//     validateFields
+// ], putCiudad);
 
-router.patch('/:id',patchCiudad)
+// router.patch('/:id',patchCiudad)
 
-router.delete('/:id', deleteCiudad);
+// router.delete('/:id', deleteCiudad);
 
-export default router;
+// export default router;
